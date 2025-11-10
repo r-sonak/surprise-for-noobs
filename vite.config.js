59ef54f1faa1ad40b4418426a89e45e19a9ee0ba
r-sonak <rss359@cornell.edu>
2025-11-09 22:48:45 -0500

    addded comment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 this is crucial for Vercel and other static hosts
   server: {
-    host: true, // Listen on all addresses
+    host: true,
     port: 5173,
   },
+  build: {
+    outDir: 'dist', // 👈 make sure Vercel finds your built files here
+  },
 })
+//test
